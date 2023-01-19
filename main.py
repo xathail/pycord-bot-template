3d5164fd6ed67e76e6f7878805286035d7ef2a50
xzerios <108062540+xzerios@users.noreply.github.com>
2023-01-19 16:20:30 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,14 @@
 import discord
-import discord
-
 
 bot = discord.Bot()
 
 @bot.command()
-
 async def slashCommandName( #Write your slash command's name
-
   ctx,
-
  # option: discord.Option(discord.SlashCommandOptionType.string) // This is if you want to add arguments / options to your commands
 
 ):
+  # Your command here
   
-  
-  
-
-
+ 
 bot.run("TOKEN")
