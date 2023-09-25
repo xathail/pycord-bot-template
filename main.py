8038a928216b482b09312a3114a855a15c3b96eb
xzerios <108062540+xzerios@users.noreply.github.com>
2023-09-25 11:56:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ bot = discord.Bot()
 @bot.command()
 async def slashCommandName( #Write your slash command's name
   ctx,
- # option: discord.Option(discord.SlashCommandOptionType.string) // This is if you want to add arguments / options to your commands
-
 ):
   # Your command here
   
